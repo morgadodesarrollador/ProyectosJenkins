58d5c94ad1c0761749e8c32c642dabd0135135ad
morgadodesarrollador <morgadodesarrollador@gmail.com>
2023-01-29 16:04:34 +0100

    Parent-dslJob-pTienda

--- proyectos/pTienda/dslTienda.groovy
+++ proyectos/pTienda/dslTienda.groovy
@@ -1,12 +1,12 @@
 // import jenkins-auto-approve-script.groovy
 
-job('groovy-Job-DSL') {
+job('Job-pTienda') {
     description('Job Padre para crear una tarea hija que despliegue el propyecto Tienda')
 
 
 
     scm {
-        git('https://github.com/morgadodesarrollador/ShellScripts.git', 'master'){ 
+        git('https://github.com/morgadodesarrollador/jenkinsPTienda.git', 'master'){ 
             node -> 
                 node / gitConfigName('morgadodesarrollador')
                 node / gitConfigEmail('morgadodesarrollador@gmail.com')
